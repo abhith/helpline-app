87abef8021f708e48ce0d463251650acd1197b07
Abhith Rajan <dearabhith@gmail.com>
2018-08-17 16:12:55 +0530

    event list item icon changed

--- helpline_app/lib/events/events_list.dart
+++ helpline_app/lib/events/events_list.dart
@@ -13,16 +13,8 @@ class EventsList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return new Scaffold(
       appBar: new AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: new Text('Events'),
       ),
       body: SafeArea(
@@ -50,16 +42,14 @@ class EventsList extends StatelessWidget {
       title: Row(
         children: <Widget>[
           Expanded(
-              child: Text(
-            document['name'],
-            style: Theme.of(context).textTheme.headline,
-          ))
+            child: Text(
+              document['name'],
+              style: Theme.of(context).textTheme.headline,
+            ),
+          )
         ],
       ),
-      leading: Icon(
-        Icons.warning,
-        color: Colors.redAccent,
-      ),
+      leading: Icon(Icons.contact_phone, color: Colors.red),
       trailing: Icon(Icons.arrow_forward),
       onTap: (() => Navigator.push(
             context,
