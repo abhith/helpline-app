1a2bdca539acdf27cdd25acceefbe82e14551a23
Abhith Rajan <dearabhith@gmail.com>
2018-08-17 11:58:05 +0530

    loading indicator updated Number of helplines indicator

--- helpline_app/lib/contacts/contact_list.dart
+++ helpline_app/lib/contacts/contact_list.dart
@@ -1,42 +1,39 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:helpline_app/common/loading_widget.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 class ContactList extends StatelessWidget {
   final String eventId;
-
-  const ContactList({Key key, this.eventId}) : super(key: key);
+  ContactList({Key key, this.eventId}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return new Scaffold(
-        appBar: new AppBar(
-          // Here we take the value from the MyHomePage object that was created by
-          // the App.build method, and use it to set our appbar title.
-          title: new Text('Helplines'),
-        ),
-        body: StreamBuilder(
-          stream: Firestore.instance
-              .collection('events')
-              .document(eventId)
-              .collection('contacts')
-              .orderBy('name')
-              .snapshots(),
-          builder: (context, snapshot) {
-            if (!snapshot.hasData) return const CircularProgressIndicator();
-            return ListView.builder(
+    return StreamBuilder(
+        stream: Firestore.instance
+            .collection('events')
+            .document(eventId)
+            .collection('contacts')
+            .orderBy('name')
+            .snapshots(),
+        builder: (context, snapshot) {
+          if (!snapshot.hasData) {
+            return LoadingWidget(
+              visible: true,
+            );
+          }
+          return new Scaffold(
+            appBar: new AppBar(
+              title: Text(
+                  'Helplines (${snapshot.data.documents.length.toString()})'),
+            ),
+            body: ListView.builder(
               itemCount: snapshot.data.documents.length,
               itemBuilder: (context, index) =>
                   _buildListItem(context, snapshot.data.documents[index]),
-            );
-          },
-        ));
+            ),
+          );
+        });
   }
 
   Widget _buildListItem(BuildContext context, DocumentSnapshot document) {
@@ -45,7 +42,7 @@ class ContactList extends StatelessWidget {
       subtitle: Text(document['phone']),
       trailing: _itemAction(context, document),
       leading: CircleAvatar(
-        child: Icon(Icons.person),
+        child: Text(document['name'][0]),
       ),
     );
   }
